cfbece309b9058a5959f280fe56c6d1ea9974495
Prathmesh Sadake <prathmeshsadake@gmail.com>
2025-04-05 09:51:30 +0530

    Increased logo side

--- src/app/(users)/layout.tsx
+++ src/app/(users)/layout.tsx
@@ -12,10 +12,10 @@ const UsersLayout = async ({ children }: { children: React.ReactNode }) => {
   });
   return (
     <div className='h-auto w-full max-w-md mx-auto'>
-      <div className='h-16 px-3 flex justify-between items-center'>
+      <div className='h-24 px-3 flex justify-between items-center'>
         <img
           src='https://1p7ctab0bz.ufs.sh/f/LSctCnwEvjMcbjULPdTkFKWqywc8i6h2PtmJBgXDVeLSMrla'
-          className='w-auto h-16 object-contain'
+          className='w-auto h-24 object-contain'
         />
 
         <Link href={"/shopping-list"}>
